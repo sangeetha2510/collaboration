74ee5b025e58f29059d37ff9e7acc6750e8c90db
hp <hp@192.168.1.8>
2018-02-06 13:03:43 +0530

    frontend commit

---
+++ collaboration_frontend/WebContent/js/app.js
@@ -0,0 +1,14 @@
+/**
+ * Angular Module
+ */
+var app=angular.module("app",['ngRoute'])
+app.config(function($routeProvider){
+	$routeProvider
+	.when('/register',{
+		templateUrl:'views/registrationform.html',
+		controller:'UserController'
+	})
+	.otherwise({
+		templateUrl:'views/home.html'
+	})
+})
